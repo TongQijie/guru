49be4be66c1a0900feac55f2618316ab657a1095
tongqijie <tongqijie@hotmail.com>
2018-03-24 22:13:19 +0800

    dev

--- test/AspNetCoreAppII/Startup.cs
+++ test/AspNetCoreAppII/Startup.cs
@@ -17,38 +17,32 @@ namespace AspNetCoreAppII
     {
         public int Run(string[] args)
         {
-            new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls(DependencyContainer.Resolve<IApplicationConfiguration>()?.Urls ?? new string[] { "http://localhost:5000" })
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureServices(x => x.AddSingleton<IHttpContextAccessor, HttpContextAccessor>())
-                .Configure(x =>
-                {
-                    HttpContextUtils.Configure(x.ApplicationServices.GetRequiredService<IHttpContextAccessor>());
-                    x.UseMiddleware<AspNetCoreAppInstance>();
-                })
-                .Build()
-                .Run();
-
+            Host.Run();
             return 0;
         }
 
         public async Task<int> RunAsync(string[] args)
         {
-            await new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls(DependencyContainer.Resolve<IApplicationConfiguration>()?.Urls ?? new string[] { "http://localhost:5000" })
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureServices(x => x.AddSingleton<IHttpContextAccessor, HttpContextAccessor>())
-                .Configure(x =>
-                {
-                    HttpContextUtils.Configure(x.ApplicationServices.GetRequiredService<IHttpContextAccessor>());
-                    x.UseMiddleware<AspNetCoreAppInstance>();
-                })
-                .Build()
-                .RunAsync();
-
+            await Host.RunAsync();
             return 0;
+        }
+
+        private IWebHost Host
+        {
+            get
+            {
+                return new WebHostBuilder()
+                    .UseKestrel()
+                    .UseUrls(DependencyContainer.Resolve<IApplicationConfiguration>()?.Urls ?? new string[] { "http://localhost:5000" })
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .ConfigureServices(x => x.AddSingleton<IHttpContextAccessor, HttpContextAccessor>())
+                    .Configure(x =>
+                    {
+                        HttpContextUtils.Configure(x.ApplicationServices.GetRequiredService<IHttpContextAccessor>());
+                        x.UseMiddleware<AspNetCoreAppInstance>();
+                    })
+                    .Build();
+            }
         }
     }
 }
