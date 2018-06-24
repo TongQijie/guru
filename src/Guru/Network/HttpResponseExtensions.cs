403ab29acb14349d1064b30b2e43ceb54f532f52
tongqijie <tongqijie@hotmail.com>
2018-06-24 13:38:08 +0800

    dev

---
+++ src/Guru/Network/HttpResponseExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Guru.Network.Abstractions;
+
+namespace Guru.Network
+{
+    public static class HttpResponseExtensions
+    {
+        public static bool IsOk(this IHttpResponse response)
+        {
+            return response != null && response.StatusCode == 200;
+        }
+    }
+}
