44dc44c6456b777034e618cf33cb923cff0e4445
qjtong <qjtong@Ctrip.com>
2018-07-10 21:50:56 +0800

    dev

---
+++ src/Guru/Utils/FiUtils.cs
@@ -0,0 +1,21 @@
+﻿using Guru.FastInfoset;
+using System.IO;
+using System.Xml;
+
+namespace Guru.Utils
+{
+    public static class FiUtils
+    {
+        public static string Read(byte[] data)
+        {
+            var doc = new XmlDocument();
+            using (var inputStream = new MemoryStream(data))
+            {
+                XmlReader fiReader = XmlReader.Create(new FIReader(inputStream), null);
+                doc.Load(fiReader);
+                fiReader.Close();
+            }
+            return doc.OuterXml;
+        }
+    }
+}
---
+++ src/Guru/Utils/GzipUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Guru.Utils
+{
+    public static class GzipUtils
+    {
+        public static byte[] Decompress(byte[] data)
+        {
+            using (var inputStream = new MemoryStream(data))
+            {
+                using (var decompressedStream = new MemoryStream())
+                {
+                    using (GZipStream gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                    {
+                        var buffer = new byte[1024 * 8];
+                        var count = 0;
+                        while ((count = gzipStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            decompressedStream.Write(buffer, 0, count);
+                        }
+                    }
+
+                    return decompressedStream.ToArray();
+                }
+            }
+        }
+    }
+}
