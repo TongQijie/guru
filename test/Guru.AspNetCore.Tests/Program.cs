9c43a11d9802c8be6eeed767a8462f2be6d6b7c6
tongqijie <tongqijie@hotmail.com>
2018-08-31 22:54:24 +0800

    dev

---
+++ test/Guru.AspNetCore.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guru.AspNetCore.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guru.Executable.AppInstance.Default.RunAsync(args);
+        }
+    }
+}
